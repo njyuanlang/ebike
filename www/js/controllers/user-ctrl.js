f3a64df8213e5233118941b0ecda714dbdc0eaff
Guan Bo <guanbo2002@gmail.com>
2015-03-14 13:24:25 +0800

    Update: register submit to serve

--- www/js/controllers/user-ctrl.js
+++ www/js/controllers/user-ctrl.js
@@ -2,9 +2,9 @@ controllers
 
 .controller('LoginCtrl', function($scope, $rootScope, $state, ActiveBLEDevice, $timeout, $window) {
   
-  $scope.loginData = {}
+  $scope.entity = {}
   
-  $scope.tryLogin = function (loginData) {
+  $scope.tryLogin = function (entity) {
     $rootScope.online = true
     $scope.device = ActiveBLEDevice.get()
     if($scope.device) {
@@ -18,7 +18,7 @@ controllers
     }
   }
 
-  $scope.goRegister = function (loginData) {
+  $scope.goRegister = function (entity) {
     $state.go('register')
   }
   
@@ -34,9 +34,9 @@ controllers
   }
 })
 
-.controller('RegisterCtrl', function($scope, $state, $interval) {
+.controller('RegisterCtrl', function($scope, $state, $interval, $ionicLoading, User, $localstorage) {
   
-  $scope.entity = {}
+  $scope.entity = {username: "13357828347", password: "123456"}
   $scope.validprompt = "获取验证码"
   
   $scope.getValidcode = function () {
@@ -55,7 +55,28 @@ controllers
   }
   
   $scope.tryRegister = function () {
-    
+    var entity = $scope.entity
+    entity.email = entity.username+"@example.com"
+    entity.realm = "client"
+
+    $ionicLoading.show({
+      template: '<i class="icon ion-loading-c ion-loading padding"></i>正在注册新账户...'
+    })
+
+    User.create(entity, function (user) {
+      User.login(entity, function (accessToken) {
+        $ionicLoading.show({
+          template: '<i class="icon ion-ios7-checkmark-outline padding"></i>注册账户成功',
+          duration: 1000
+        })
+        $localstorage.setObject('$EBIKE$LoginData', $scope.entity)
+      })
+    }, function (res) {
+      $ionicLoading.show({
+        template: '<i class="icon ion-ios7-close-outline padding"></i>手机已经存在，请换其他手机',
+        duration: 2000
+      })
+    })
   }
 })
 
