067c08888821720893baf2b6861c537c6c00f4be
Guan Bo <guanbo2002@gmail.com>
2015-09-29 19:38:45 +0800

    Update: chats pull sync

--- www/js/controllers/chats-ctrl.js
+++ www/js/controllers/chats-ctrl.js
@@ -1,13 +1,30 @@
 controllers
 
-.controller('ChatsCtrl', function ($scope, $state, Message, RemoteStorage) {
+.controller('ChatsCtrl', function ($scope, $state, Message, RemoteStorage, $timeout) {
   
-  Message.chats(function (results) {
-    $scope.chats = results;
-    results.forEach(function (item) {
-      RemoteStorage.getAvatar(item._id).success(function (buffer) {
-        item.avatar = buffer;
+  var syncTimer = null;
+  var syncDone = function () {
+    if(syncTimer) {
+      $timeout.cancel(syncTimer);
+      syncTimer = null;
+    }
+    $scope.$broadcast('scroll.refreshComplete');
+  }
+  $scope.sync = function () {
+    Message.chats(function (results) {
+      $scope.chats = results;
+      results.forEach(function (item) {
+        RemoteStorage.getAvatar(item._id).success(function (buffer) {
+          item.avatar = buffer;
+        });
       });
+      syncDone();
+    }, function (reason) {
+      syncDone();
     });
-  });
+    
+    syncTimer = $timeout(syncDone, 5000);
+  }
+  
+  $scope.sync();
 })
