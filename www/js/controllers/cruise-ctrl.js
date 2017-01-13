09737937fb37d350f22ce26dbdfe7dcb41a702d0
Guan Bo <guanbo2002@gmail.com>
2017-01-13 18:26:18 +0800

    Update: 5x

--- www/js/controllers/cruise-ctrl.js
+++ www/js/controllers/cruise-ctrl.js
@@ -36,7 +36,15 @@ controllers
     if(!$scope.online) return $scope.device.onConnected()
 
     $scope.promptRotate = Math.abs(window.orientation) !== 90
-    // $scope.promptRotate = false
+    $scope.promptRotate = false;
+    var ratio = window.devicePixelRatio || 1;
+    $scope.screen = {
+      w: window.screen.width*ratio,
+      h: window.screen.height*ratio,
+      w2: screen.width,
+      h2: screen.height
+    }
+    console.log(JSON.stringify($scope.screen));
 
     $scope.workmodeIdx = workmodes.indexOf($scope.device.bike.workmode);
   }
